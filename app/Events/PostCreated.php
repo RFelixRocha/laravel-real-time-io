5e53897b3a45bd533732322121b935117c51d4a7
RFelixRocha <felixrochaeso@gmail.com>
2021-04-20 04:51:59 -0400

    Comfigurando o tipo de channel

--- app/Events/PostCreated.php
+++ app/Events/PostCreated.php
@@ -11,7 +11,7 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class PostCreated
+class PostCreated implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
@@ -33,6 +33,6 @@ class PostCreated
      */
     public function broadcastOn()
     {
-        return new PrivateChannel('channel-name');
+        return new Channel('post-created');
     }
 }
