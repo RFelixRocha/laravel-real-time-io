c9205bc159f5cd15c42d3e301525ddd18c7c081f
RFelixRocha <felixrochaeso@gmail.com>
2021-04-20 04:39:56 -0400

    Criando a parte de event, listener e observer para a parte de Post.

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,10 +2,13 @@
 
 namespace App\Providers;
 
+use App\Models\Post;
+use App\Observers\PostObserver;
+use App\Events\{PostCreated};
+use App\Listeners\{NotifyUsersNewPostCreated};
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -18,6 +21,10 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+
+        PostCreated::class => [
+            NotifyUsersNewPostCreated::class
+        ]
     ];
 
     /**
@@ -27,6 +34,6 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Post::observe(PostObserver::class);
     }
 }
